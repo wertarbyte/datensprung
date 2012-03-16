c173eb487c0d1fb155c8e2ab2cbaeb4a73fa04e3
Stefan Tomanek <stefan@pico.ruhr.de>
2012-03-16 14:29:31 +0100

    add output pin switching command

--- ds_cmd.h
+++ ds_cmd.h
@@ -1,3 +1,4 @@
 /* print string payload to serial */
 #define DS_CMD_SERIAL_STRING	0x01
+/* set output pins */
 #define DS_CMD_OUTPUT_PINS	0x02
--- process.c
+++ process.c
@@ -14,12 +14,19 @@ static void cmd_serial_string(struct ds_frame_t *p) {
 	/* this operation is not idem-potent, so we filter out retransmissions
 	 * by using a sequence number as first byte of the payload
 	 */
-	if (p->data[0] == last_str_seq) break;
+	if (p->data[0] == last_str_seq) return;
 	last_str_seq = p->data[0];
 	for (uint8_t i=1; i<DS_FRAME_PAYLOAD_SIZE && p->data[i]; i++) {
 		serial_write(p->data[i]);
 	}
 	// serial_write('\n');
+}
+
+static void cmd_output_pins(struct ds_frame_t *p) {
+	/* switch output pins */
+	uint8_t levels = p->data[0]; // set these pins to high
+	/* we use the entire PB-Register */
+	PORTB = levels;
 }
 
 void process_packet(struct ds_frame_t *p) {
@@ -32,6 +39,9 @@ void process_packet(struct ds_frame_t *p) {
 	switch (p->cmd) {
 		case DS_CMD_SERIAL_STRING: /* print characters */
 			cmd_serial_string(p);
+			break;
+		case DS_CMD_OUTPUT_PINS:
+			cmd_output_pins(p);
 			break;
 	}
 }
